f22b818a22b18eb1fa2ecf126816c9ec2d1834b0
Eunjung Jenny Jeon <jenny.it.kr@gmail.com>
2020-02-14 10:47:29 +0900

    Solve 탑 using stack

--- python/탑.py
+++ python/탑.py
@@ -1,15 +1,30 @@
+# 완전 탐색
+# def solution(heights):
+#     answer = []
+#     for i in range(len(heights)):
+#         if i == 0:
+#             answer.append(0)
+#         else:
+#             stack = heights[:i]
+#             while stack:
+#                 if stack.pop() > heights[i]:
+#                     answer.append(len(stack)+1)
+#                     break
+#             else:
+#                 answer.append(0)
+                
+#     return answer
+
+# 스택
 def solution(heights):
     answer = []
     for i in range(len(heights)):
-        if i == 0:
-            answer.append(0)
+        stack = []
+        for j in range(i):
+            if heights[j] > heights[i]:
+                stack.append(j+1)
+        if stack:
+            answer.append(stack.pop())
         else:
-            stack = heights[:i]
-            while stack:
-                if stack.pop() > heights[i]:
-                    answer.append(len(stack)+1)
-                    break
-            else:
-                answer.append(0)
-                
+            answer.append(0)
     return answer

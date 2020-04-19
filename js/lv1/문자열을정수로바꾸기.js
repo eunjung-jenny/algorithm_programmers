ecd8c89cc5ca82dc50b9f2a1b0cc95912279a64a
jenny.kr <jenny.it.kr@gmail.com>
2020-04-19 18:47:35 +0900

    Solve lv1 문자열을정수로바꾸기

---
+++ js/lv1/문자열을정수로바꾸기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  return Number(s) * -1;
+}
+
+s = ["1234", "-1234"];
+result = [1234, -1234];
+
+// function strToInt(str) {
+//   return +str;
+// }
+
+// function strToInt(str) {
+//   return str / 1;
+// }

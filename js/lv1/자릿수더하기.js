fad8885cf1117d50ef2bb777cf719618304beb14
jenny.kr <jenny.it.kr@gmail.com>
2020-04-25 18:10:12 +0900

    Solve lv1 자릿수더하기.js

---
+++ js/lv1/자릿수더하기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  if (n < 10) {
+    return n;
+  }
+  return (n % 10) + solution(Math.floor(n / 10));
+}
+
+N = [123, 987];
+answer = [6, 24];
+
+for (let i = 0; i < N.length; i++) {
+  if (solution(N[i]) == answer[i]) {
+    console.log(`${i + 1}: pass`);
+  } else {
+    console.log(`${i + 1}: fail`);
+  }
+}
